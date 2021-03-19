15a221a3e4357a55e3727267abb5de7aa86c8734
Andrey <70208784+andreystashev@users.noreply.github.com>
2021-03-19 22:11:33 +0300

    first lesson is done

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,34 +1,42 @@
-import datetime
+from datetime import datetime, timedelta
+from random import choice
 from flask import Flask
 
 app = Flask(__name__)
+cats = ['Корниш рекс', 'Русская голубая', 'Шотландская вислоухая', 'Мэйн-Кун', 'Манчкин']
+war_words = []
 
 
-@app.route('/hello_word')
+@app.route('/hello_world')
 def test_function():
-   pass
+    return 'Привет мир!'
 
 
 @app.route('/cars')
-def test_function():
-   pass
+def test_function1():
+    return 'Chevrolet, Renault, Ford, Lada'
 
 
 @app.route('/cats')
-def test_function():
-   pass
+def test_function2():
+    return choice(cats)
 
 
 @app.route('/get_time/now')
-def test_function():
-   pass
+def test_function3():
+    return "Точное время {}".format(datetime.strftime(datetime.now(), '%H.%M.%S'))
 
 
 @app.route('/get_time/future')
-def test_function():
-   pass
+def test_function4():
+    return "Точное время через час будет {}".format((datetime.now() + timedelta(hours=1)).time())
 
 
 @app.route('/get_random_word')
-def test_function():
-   pass
+def test_function5(file_name='war_and_peace.txt'):
+    with open(file_name, 'r', encoding='cp1251') as file:
+        for line in file:
+            for word in line.split():
+                war_words.append(word)
+    file.close()
+    return choice(war_words)
