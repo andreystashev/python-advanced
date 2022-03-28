99bd5353d4793c8c6df4955b5f80e017b113db98
Andrey <70208784+andreystashev@users.noreply.github.com>
2022-03-28 23:41:07 +0300

    Delete python_advanced directory

--- python_advanced/module_05_processes_and_threads/materials/working_with_processes_from_python/subprocess_run.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def run_program():
-    res = subprocess.run(['python', 'test_program.py'], input=b'some input\notherinput')
-    print(res)
-
-if __name__ == '__main__':
-    run_program()
